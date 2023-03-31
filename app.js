c73b8170016b44cf1220e8e1b6ca0ea8124128c5
Hiroki Miyabayashi <hiroki.miya00@gmail.com>
2023-03-31 14:20:24 -0400

    added add and remove function

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-}); 
+const inputText = document.querySelector('#txt');
+const myButton = document.querySelector('.btn__add');
+const list = document.querySelector('.items');
+myButton.addEventListener('click', (e)=>{
+    if(inputText.value != ""){
+        e.preventDefault();
+        //create list
+        const myList = document.createElement('li');
+        myList.innerHTML = inputText.value;
+        list.appendChild(myList);
+        //create span
+        const mySpan = document.createElement('span');
+        mySpan.innerHTML = 'x';
+        myList.appendChild(mySpan);
+    }
+    const close = document.querySelectorAll('span');
+    for(let i=0; i<close.length; i++){
+        close[i].addEventListener('click', ()=>{
+            close[i].parentElement.style.opacity = 0;
+            setTimeout(()=>{
+                close[i].parentElement.style.display = "none";
+                close[i].parentElement.remove();
+            }, 500);
+        })
+    }
+    inputText.value = "";
+});
